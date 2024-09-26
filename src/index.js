359d53b895a866a6b67641a6f1061c3c31f35051
Dennis.B <Baust.Dennis@gmail.com>
2024-09-26 16:20:13 +0200

    bootstrap navbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Navbar from "./components/navbar";
 
-const element = <h1>Hello, World!</h1>;
+const element = <Navbar />;
 const root = createRoot(document.getElementById("root"));
 root.render(element);
