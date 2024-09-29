6961f742398976a37542aa7a1d2f0652e5eb0564
Dennis.B <Baust.Dennis@gmail.com>
2024-09-29 22:00:59 +0200

    add to shopping-cart

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,40 @@ import Product from './components/product';
 import ShoppingCart from './components/shopping-cart';
 
 class App extends Component {
-    state = {  } 
+    state = { 
+        items: []
+     } 
+
+    addItem = (amount, name, price) => {
+        let currentItems = this.state.items;
+        let existingItem = currentItems.find(item => item.name === name);
+        if (existingItem) {
+            existingItem.amount += amount;
+            existingItem.price += price;
+        } else {
+            currentItems.push({ amount, name, price });    
+        }
+        this.setState({ items: currentItems })
+        console.log(this.state.items);
+    }
+
+    removeItem = (index) => {
+        let currentItems = this.state.items;
+        currentItems.splice(index, 1);
+        this.setState({ items: currentItems })
+    }
+
     render() { 
         return  <React.Fragment>
                     <Navbar />
                     <div className="main-container">
                         <div className="product-container">
-                            <Product image="sushi.jpg" title="Sushi" description="Description..." />
-                            <Product image="salami-pizza.jpg" title="Salami Pizza" description="Description..." />
-                            <Product image="schoko-eis.png" title="Schokoladeneis" description="Description..." />
-                            <Product image="salat.png" title="Gemischter Salat" description="Description..." />
+                            <Product onAdd={() => this.addItem(1, "Sushi", 20)} image="sushi.jpg" title="Sushi" description="Description..." />
+                            <Product onAdd={() => this.addItem(1, "Salami Pizza", 15)} image="salami-pizza.jpg" title="Salami Pizza" description="Description..." />
+                            <Product onAdd={() => this.addItem(1, "Schokoladeneis", 6)} image="schoko-eis.png" title="Schokoladeneis" description="Description..." />
+                            <Product onAdd={() => this.addItem(1, "Gemischter Salat", 10)} image="salat.png" title="Gemischter Salat" description="Description..." />
                         </div>
-                        <ShoppingCart />
+                        <ShoppingCart items={this.state.items} onRemove={(index) => this.removeItem(index)} />
                     </div>
                 </React.Fragment>;
     }
--- src/components/shopping-cart.jsx
+++ src/components/shopping-cart.jsx
@@ -2,23 +2,15 @@ import React, { Component } from 'react';
 
 class ShoppingCart extends Component {
     state = { 
-        items: [{
-            amount: 1,
-            name: "Sushi",
-            price: 10
-        },
-        {
-            amount: 1,
-            name: "Salami Pizza",
-            price: 15
-        }]
+        
      } 
     render() { 
         return <div className="shopping-cart">
             <h2>Warenkorb</h2>
-            {this.state.items.map((item, index) => {
+            {this.props.items.map((item, index) => {
                 return <div className='item' key={index}>
                     <span>{item.amount} x {item.name} - {item.price}€</span>
+                    <button onClick={() => this.props.onRemove(index)}>X</button>
                 </div>
             })}
         </div>;
